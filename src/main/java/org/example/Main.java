614b1e30663e8d7caf7f2cae93ebad046388534d
JuanCarlosPerezSimarro <juan.perez71@educa.madrid.org>
2022-09-19 13:53:53 +0200

    Commit prueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("HOLA MUNDO");
     }
 }
